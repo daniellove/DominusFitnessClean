73723b07756442f7144c7527c8dfe011983a1ba2
Daniel Nelmes-Love <dnelmeslove@gmail.com>
2022-02-06 10:28:56 +1300

    Initial copy

---
+++ php-elements/menu.php
@@ -0,0 +1,6 @@
+<div id="menuEle">
+	<span onclick="window.location = '/DominusFitness/pages/signup/'">Sign Up <i class="fas fa-arrow-right"></i></span>
+	<span onclick="window.location = '/DominusFitness/pages/testimonials/'">Testimonials <i class="fas fa-arrow-right"></i></span>
+	<span onclick="window.location = '/DominusFitness/pages/about/'">About <i class="fas fa-arrow-right"></i></span>
+	<span onclick="window.location = '/DominusFitness/pages/contact/'">Contact <i class="fas fa-arrow-right"></i></span>
+</div>
